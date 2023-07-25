6f32a4e70fc60c1beba73ba9b0390aa9994c9362
BarisBeytur <beyturbaris@gmail.com>
2023-07-25 16:51:57 +0300

    Add project files.

---
+++ BusinessLayer_/Concrete/CategoryManager.cs
@@ -0,0 +1,34 @@
+﻿using BusinessLayer_.Abstract;
+using DataAccessLayer_.Abstract;
+using DataAccessLayer_.Concrete.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer_.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+
+        ICategoryDal _categorydal;
+
+        public CategoryManager(ICategoryDal categorydal)
+        {
+            _categorydal = categorydal;
+        }
+
+        public void CategoryAdd(Category category)
+        {
+            _categorydal.Insert(category);
+        }
+
+        public List<Category> GetList()
+        {
+            return _categorydal.List();
+        }
+
+    }
+}
---
+++ DataAccessLayer_/EntityFramework/EfCategoryDal.cs
@@ -0,0 +1,16 @@
+﻿using DataAccessLayer_.Abstract;
+using DataAccessLayer_.Concrete.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer_.EntityFramework
+{
+    public class EfCategoryDal : GenericRepository<Category>, ICategoryDal
+    {
+
+    }
+}
